c9b8b3fcea70bc3659e1ea9981315f3e4fb0e0b8
Rtxeon <87909941+Rtxeon@users.noreply.github.com>
2022-08-14 23:59:46 +0530

    Update SlashCommands.js

--- Handler/SlashCommands.js
+++ Handler/SlashCommands.js
@@ -10,17 +10,17 @@ module.exports = async (SynthBot) => {
             const SlashCommand = require(`../Commands/SlashCommands/${dir}/${file}`);
             if (!SlashCommand.name) return console.error(`[SLASH COMMANDS] NameError: ${file.split(".")[0]} Application command name is required.`);
             if (!SlashCommand.description) return console.error(`[SLASH COMMANDS] DescriptionError: ${file.split(".")[0]} Application command description is required.`);
-            SynthBot.SlashCommands.set(SlashCommand.name, SlashCommand);
+            SynthBot.slashCommands.set(SlashCommand.name, SlashCommand);
             console.log(`[SLASH COMMANDS] ${SlashCommand.name} Application Command Are Added For Being Registered To SynthBot`);
         }
 
         SlashCMD.push({
-            name: slashCommand.name,
-            description: slashCommand.description,
-            type: slashCommand.type,
-            options: slashCommand.options ? slashCommand.options : null,
-            dm_permission: slashCommand.dm ? slashCommand.dm : null,
-            default_member_permissions: slashCommand.member_permissions ? PermissionsBitField.resolve(slashCommand.member_permissions).toString() : null
+            name: SlashCommand.name,
+            description: SlashCommand.description,
+            type: SlashCommand.type,
+            options: SlashCommand.options ? SlashCommand.options : null,
+            dm_permission: SlashCommand.dm ? SlashCommand.dm : null,
+            default_member_permissions: SlashCommand.member_permissions ? SlashCommand.resolve(SlashCommand.member_permissions).toString() : null
         });
     });
 
